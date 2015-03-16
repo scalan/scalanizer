e7b358e0544c0cc7fcc5d68f15afbaa8d6027bf3
Maxim Gekk <gekk.maxim@huawei.com>
2015-03-16 14:24:45 +0300

    Initial commit

---
+++ project/Build.scala
@@ -0,0 +1,25 @@
+import sbt._
+import Keys._
+import sbtassembly.Plugin._
+import AssemblyKeys._
+
+object ScalanBuild extends Build {
+  lazy val sharedSettings = Defaults.coreDefaultSettings ++ Seq(
+    scalaVersion := "2.10.4",
+    crossVersion := CrossVersion.full,
+    version := "0.0.1",
+    organization := "com.huawei"
+  )
+
+  lazy val ScalanPlugin = Project(
+    id   = "scalan-plugin",
+    base = file(".")
+  ) settings (
+    sharedSettings ++ assemblySettings : _*
+  ) settings (
+    libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _ % "provided"),
+    libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-compiler" % _ % "provided"),
+    jarName in assembly := name.value + "_" + scalaVersion.value + "-" + version.value + "-assembly.jar",
+    assemblyOption in assembly ~= { _.copy(includeScala = false) }
+  )
+}
---
+++ src/main/scala/ScalanPlugin.scala
@@ -0,0 +1,27 @@
+package scalan.plugin
+
+import scala.tools.nsc.Global
+import scala.tools.nsc.plugins.{PluginComponent, Plugin}
+
+class ScalanPlugin(val global: Global) extends Plugin {
+  /** Visible name of the plugin */
+  val name: String = "scalan"
+
+  /** The compiler components that will be applied when running this plugin */
+  val components: List[PluginComponent] = List(new HelloComponent(global))
+
+  /** The description is printed with the option: -Xplugin-list */
+  val description: String = "Optimization through staging"
+
+  /** Pluging-specific options without -P:scalan:  */
+  override def processOptions(options: List[String], error: String => Unit) {
+    options foreach {
+      case "debug" => println("Debug is enabled.")
+      case option => error("Option not understood: " + option)
+    }
+  }
+
+  /** A description of the plugin's options */
+  override val optionsHelp = Some(
+    "  -P:"+ name +":debug     output debug information")
+}
