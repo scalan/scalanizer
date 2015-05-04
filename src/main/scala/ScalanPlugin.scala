2957f1b113076c60bc6d72aed71fa94806249f23
Maxim Gekk <gekk.maxim@huawei.com>
2015-05-04 11:34:47 +0300

    The config method is enabled.

--- src/main/scala/ScalanPlugin.scala
+++ src/main/scala/ScalanPlugin.scala
@@ -5,7 +5,7 @@ import scala.tools.nsc._
 import scala.tools.nsc.plugins.{PluginComponent, Plugin}
 import scala.reflect.internal.util.BatchSourceFile
 import scalan.meta.ScalanAst._
-import scalan.meta.ScalanParsers
+import scalan.meta.{CodegenConfig, ScalanParsers}
 
 class ScalanPluginComponent(val global: Global)
   extends PluginComponent with ScalanParsers with Enricher with GenScalaAst {
@@ -128,6 +128,8 @@ class ScalanPluginComponent(val global: Global)
     val serialized = global.Literal(Constant(str))
     q"val serializedMetaAst = $serialized"
   }
+
+  def config: CodegenConfig = ScalanPluginConfig.codegenConfig
 }
 
 class ScalanPlugin(val global: Global) extends Plugin {
