eb47a7d995e4264a33c5f95fe377fe88defefe94
Maxim Gekk <gekk.maxim@huawei.com>
2015-06-05 09:59:05 +0300

    Generating of Hot Spot Kernels and Manager

--- src/main/scala/HotSpots.scala
+++ src/main/scala/HotSpots.scala
@@ -28,4 +28,48 @@ trait HotSpots {
 
     hotSpotTransformer.transform(unitBody)
   }
+
+  def getHotSpotKernels = {
+    q"""
+      object HotSpotKernels {
+        import java.io.File
+        import scalan.compilation.GraphVizConfig
+
+        lazy val ddmvmKernel = {
+          val ctx = HotSpotManager.getScalanContext
+          val compilerOutput = ctx.buildExecutable(
+            new File("./"),
+            "ddmvm",
+             ctx.ddmvmWrapper, GraphVizConfig.default)(ctx.defaultCompilerConfig)
+          val (cls, method) = ctx.loadMethod(compilerOutput)
+          val instance = cls.newInstance().asInstanceOf[((Array[Array[Double]], Array[Double])) => Array[Double]]
+               instance
+        }
+      }
+    """
+  }
+
+  def getHotSpotManager = {
+    q"""
+      object HotSpotManager {
+        import scalan.ScalanCommunityDslExp
+        import scalan.compilation.lms.{CommunityLmsBackend, CoreBridge}
+        import scalan.compilation.lms.scalac.CommunityLmsCompilerScala
+        import scalan.primitives.EffectfulCompiler
+        import paradise.linalgebra.implOfLinearAlgebra.StagedEvaluation.LinearAlgebraDslExp
+
+        lazy val scalanContext = new Scalan
+        def getScalanContext = scalanContext
+
+        class Scalan extends LinearAlgebraDslExp with CommunityLmsCompilerScala with CoreBridge
+          with ScalanCommunityDslExp with EffectfulCompiler {
+
+          lazy val ddmvmWrapper = fun(((in: Rep[scala.Tuple2[Array[Array[Double]], Array[Double]]]) => {
+            LA.ddmvm(in._1, in._2)
+          }))
+          val lms = new CommunityLmsBackend
+        }
+      }
+    """
+  }
 }
