c7ee782ab7dc08e0c660ae0558ec1ba8677381cf
Maxim Gekk <gekk.maxim@huawei.com>
2015-06-16 18:19:18 +0300

    Bug fix: force using of sbt for kernel compilation

--- src/main/scala/HotSpots.scala
+++ src/main/scala/HotSpots.scala
@@ -71,7 +71,7 @@ trait HotSpots extends Enricher with Backend with ScalanParsers {
         lazy val ${TermName(method.name + "Kernel")} = {
           val ctx = HotSpotManager.${TermName(scalanContextGetter)}
           val compilerOutput = ctx.buildExecutable(
-            new File("./"),
+            new File("./it-out/" + ${method.name}),
             ${Literal(Constant(method.name))},
             ctx.${TermName(method.name + "Wrapper")}, GraphVizConfig.default)(ctx.CompilerConfig(Some("2.11.2"), Seq.empty))
           val (cls, method) = ctx.loadMethod(compilerOutput)
