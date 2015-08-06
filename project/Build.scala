fc1915f77616089ac26a4a7eae11c8ae74a575c6
Maxim Gekk <gekk.maxim@huawei.com>
2015-08-06 11:01:33 +0300

    Switching on scala virtualized 2.11.2

--- project/Build.scala
+++ project/Build.scala
@@ -5,8 +5,9 @@ import AssemblyKeys._
 
 object ScalanBuild extends Build {
   lazy val sharedSettings = Defaults.coreDefaultSettings ++ Seq(
-    scalaVersion := "2.11.6",
- //   crossVersion := CrossVersion.full,
+//    scalaVersion := "2.11.6",
+    scalaOrganization := "org.scala-lang.virtualized",
+    scalaVersion := "2.11.2",
     version := "0.0.2",
     organization := "com.huawei"
   )
@@ -17,11 +18,11 @@ object ScalanBuild extends Build {
   ) settings (
     sharedSettings ++ assemblySettings : _*
   ) settings (
-    libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _ % "provided"),
-    libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-compiler" % _ % "provided"),
+    libraryDependencies <+= (scalaVersion)("org.scala-lang.virtualized" % "scala-reflect" % _ % "provided"),
+    libraryDependencies <+= (scalaVersion)("org.scala-lang.virtualized" % "scala-compiler" % _ % "provided"),
     libraryDependencies ++= Seq("com.huawei.scalan" %% "scalan-common" % "0.2.9-SNAPSHOT"),
     libraryDependencies ++= Seq("com.huawei.scalan" %% "scalan-core" % "0.2.9-SNAPSHOT"),
-    libraryDependencies ++= Seq("com.huawei.scalan" %% "scalan-meta" % "0.2.9-SNAPSHOT"),
+    libraryDependencies ++= Seq("com.huawei.scalan" %% "scalan-meta" % "0.2.9-SNAPSHOT" exclude("org.scala-lang", "scala-compiler")),
     publishArtifact in (Compile, packageBin) := false,
     assemblyOption in assembly ~= { _.copy(includeScala = false, includeDependency = true) },
     artifact in (Compile, assembly) := {
