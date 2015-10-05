1ecf38d82b45dc093f4eec6e75ed46e541e3c848
Maxim Gekk <gekk.maxim@huawei.com>
2015-10-05 11:16:42 +0300

    Additional comments are added to Scalanizer's config.

--- src/main/scala/ScalanPluginConfig.scala
+++ src/main/scala/ScalanPluginConfig.scala
@@ -3,10 +3,34 @@ package scalan.plugin
 import scalan.meta.CodegenConfig
 
 object ScalanPluginConfig {
+  /** The folder where the app is located and where the generated code will be stored. */
+  val home = "/home/mgekk/scalan/scalanizer-demo"
+  /** The flag indicates that generated code (virtualized code, boilerplate and type wrappers)
+    * should be stored on the file system. */
   var save: Boolean = true
+  /** Reload virtualized code from the file system. */
   var read: Boolean = true
+  /** The flag indicates that the plugin has to generate additional information and to store it
+    * the debug folder and outputs to the console. */
   var debug: Boolean = false
+  /** The flag indicates that Meta AST of entities should be serialized and assigned to some variable
+    * inside virtualized code. */
   var saveMetaAst: Boolean = false
+  /** Mapping of entities and their concrete classes. */
+  val entities = Map[String, Set[String]](
+    "LinearAlgebra" -> Set(),
+    "Num" -> Set("DoubleNum"),
+    "NumMonoid" -> Set("PlusMonoid"),
+    "Col" -> Set("ColOverArray", "PairCol"),
+    "Vec" -> Set("DenseVec"),
+    "Matr" -> Set("DenseMatr"),
+    "MatrOp" -> Set("BaseMatrOp"),
+    "LinearAlgebraOp" -> Set("LA")
+  )
+  /** The types that shouldn't be Rep[]. */
+  val typeClasses = List("Elem", "Cont", "ClassTag")
+
+  /** Config for Scalan META. */
   val codegenConfig = CodegenConfig(
     name = "Scalan Plugin",
     srcPath = "/",
@@ -30,18 +54,6 @@ object ScalanPluginConfig {
     entityTypeSynonyms = Map[String, String](),
     isAlreadyRep = false,
     isSeqEnabled = false
-  )
-  val typeClasses = List[String]("Elem", "Cont", "ClassTag")
-  val home = "/home/mgekk/scalan/scalanizer-demo"
-  val entities = Map[String, Set[String]](
-    "LinearAlgebra" -> Set(),
-    "Num" -> Set("DoubleNum"),
-    "NumMonoid" -> Set("PlusMonoid"),
-    "Col" -> Set("ColOverArray", "PairCol"),
-    "Vec" -> Set("DenseVec"),
-    "Matr" -> Set("DenseMatr"),
-    "MatrOp" -> Set("BaseMatrOp"),
-    "LinearAlgebraOp" -> Set("LA")
   )
 }
 
